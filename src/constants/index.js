27ab6568b376a5b93a5f9d44db163a8f1f702c6c
jsymondst <jammiesymondstayler@gmail.com>
2020-08-15 11:34:54 +0100

    pointing to heroku backend

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,7 @@
-export const API_ROOT = "http://localhost:3001";
-export const API_WS_ROOT = "ws://localhost:3001/cable";
+export const API_ROOT = "http://bhg-backend.herokuapp.com";
+export const API_WS_ROOT = "wss://bhg-backend.herokuapp.com/cable";
+// export const API_ROOT = "http://localhost:3001";
+// export const API_WS_ROOT = "ws://localhost:3001/cable";
 export const HEADERS = {
     "Content-Type": "application/json",
     Accept: "application/json",
