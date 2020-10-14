8e819e1a28230f010a87e4c7f469ad5cf3810053
jammiesymondstayler@gmail.com <jammiesymondstayler@gmail.com>
2020-10-14 16:50:30 +0100

    updated C4 draw logic and reporting)

--- src/features/connectFour/ConnectFour.js
+++ src/features/connectFour/ConnectFour.js
@@ -4,171 +4,165 @@ import { ActionCable } from "react-actioncable-provider";
 import { Icon, Segment } from "semantic-ui-react";
 
 import {
-    connectFourPlace,
-    connectFourReset,
-    selectConnectFourGrid,
-    selectConnectFourTurn,
-    selectConnectFourWinner,
+  connectFourPlace,
+  connectFourReset,
+  selectConnectFourGrid,
+  selectConnectFourTurn,
+  selectConnectFourWinner,
 } from "./connectFourSlice";
 
 import {
-    selectActiveGameID,
-    setPlayer,
-    selectPlayerNumber,
+  selectActiveGameID,
+  setPlayer,
+  selectPlayerNumber,
 } from "../activeGame/activeGameSlice";
 
 import { sendTurn } from "../../constants";
 import PlayerControls from "../../components/GameView/PlayerControls";
 
 export const ConnectFour = () => {
-    const dispatch = useDispatch();
-    const grid = useSelector(selectConnectFourGrid);
-    const turn = useSelector(selectConnectFourTurn);
-    const activeGameID = useSelector(selectActiveGameID);
-    const playerNumber = useSelector(selectPlayerNumber);
-    const winner = useSelector(selectConnectFourWinner);
+  const dispatch = useDispatch();
+  const grid = useSelector(selectConnectFourGrid);
+  const turn = useSelector(selectConnectFourTurn);
+  const activeGameID = useSelector(selectActiveGameID);
+  const playerNumber = useSelector(selectPlayerNumber);
+  const winner = useSelector(selectConnectFourWinner);
 
-    const iconDetails = {
-        "1": { color: "yellow", icon: "circle" },
-        "2": { color: "red", icon: "circle" },
+  const iconDetails = {
+    1: { color: "yellow", icon: "circle" },
+    2: { color: "red", icon: "circle" },
+  };
+
+  const substituteIcon = (piece, size) => {
+    const selectIcon = {
+      red: <Icon name="circle" color="red" size={size} />,
+      yellow: <Icon name="circle" color="yellow" size={size} />,
     };
+    return selectIcon[piece];
+  };
 
-    const substituteIcon = (piece, size) => {
-        const selectIcon = {
-            red: <Icon name="circle" color="red" size={size} />,
-            yellow: <Icon name="circle" color="yellow" size={size} />,
-        };
-        return selectIcon[piece];
-    };
+  const myTurn = () => {
+    return playerNumber === -1 ? true : Number(turn) === Number(playerNumber);
+    // return Number(turn) === Number(playerNumber);
+  };
 
-    const myTurn = () => {
-        return playerNumber === -1 ? true : (Number(turn) === Number(playerNumber))
-        // return Number(turn) === Number(playerNumber);
-    };
+  const drawGrid = () => {
+    return (
+      <div className="c4Board">
+        {grid.map((column, posX) => {
+          const gridColumn = column.map((piece, posY) => {
+            return drawCell(piece, posX, posY);
+          });
 
-    const drawGrid = () => {
-        return (
-            <div className="c4Board">
-                {grid.map((column, posX) => {
-                    const gridColumn = column.map((piece, posY) => {
-                        return drawCell(piece, posX, posY);
-                    });
+          gridColumn.reverse();
 
-                    gridColumn.reverse();
-
-                    return (
-                        <div
-                            className={
-                                myTurn() ? "c4Column active" : "c4Column"
-                            }
-                        >
-                            {gridColumn}
-                        </div>
-                    );
-                })}
+          return (
+            <div className={myTurn() ? "c4Column active" : "c4Column"}>
+              {gridColumn}
             </div>
-        );
-    };
+          );
+        })}
+      </div>
+    );
+  };
 
-    const drawCell = (piece, posX, posY) => {
-        const assignTileHandler = () => {
-            if (myTurn()) {
-                return () => handleTileClick(posX);
-            }
-        };
-
-        return (
-            <div
-                className={"c4cell"}
-                key={`${posX}, ${posY}`}
-                onClick={assignTileHandler()}
-            >
-                {piece !== "blank" ? substituteIcon(piece, "huge") : null}
-            </div>
-        );
-    };
-
-    const pieceSwitch = (turn) => {
-        const pieceSwitchKey = {
-            "1": "yellow",
-            "2": "red",
-        };
-        return pieceSwitchKey[turn];
-    };
-
-    const handleTileClick = (columnIndex) => {
-        if (!winner) {
-
-
-
-            const payload = {
-                columnIndex,
-                piece: pieceSwitch(turn),
-            };
-
-            if (playerNumber === -1) {
-                dispatch(connectFourPlace(payload));
-            } else {
-                const action = {
-                    action: "place",
-                    payload,
-                };
-                sendTurn(activeGameID, action, "connectFour", null);
-            }
-            
-        }
-    };
-
-    const handleReceivedTurn = (response) => {
-        console.table(response);
-        // console.log("triggered.");
-        const { turn } = response;
-        if (response.turn && response.turn.game_type === "connectFour") {
-            const action = JSON.parse(turn.action);
-            switch (action.action) {
-                case "place":
-                    dispatch(connectFourPlace(action.payload));
-                    break;
-                case "reset":
-                    dispatch(connectFourReset());
-                    dispatch(setPlayer(null));
-                    break;
-                default:
-                    console.error("invalid connectFour turn received");
-            }
-        }
-    };
-
-    const headerSection = () => {
-        if (!playerNumber) {
-            return <h1>Select a player</h1>;
-        } else if (winner) {
-            return (
-                <h1>
-                    {substituteIcon(winner, "big")}
-                    {` Wins!`}
-                </h1>
-            );
-        } else {
-            return <h1>{substituteIcon(pieceSwitch(turn), "big")}</h1>;
-        }
+  const drawCell = (piece, posX, posY) => {
+    const assignTileHandler = () => {
+      if (myTurn()) {
+        return () => handleTileClick(posX);
+      }
     };
 
     return (
-        <Segment.Group>
-            <PlayerControls myTurn={myTurn()} iconDetails={iconDetails} />
-            <Segment padded centered>
-                <ActionCable
-                    channel={{ channel: "TurnsChannel", game_id: activeGameID }}
-                    onReceived={handleReceivedTurn}
-                    onConnected={() => console.log("C4 Turn Channel Connected")}
-                    onDisconnected={() => console.log("C4 Turn Channel DCed")}
-                />
-                {headerSection()}
-                <div className="boardbox">{drawGrid()}</div>
-            </Segment>
-        </Segment.Group>
+      <div
+        className={"c4cell"}
+        key={`${posX}, ${posY}`}
+        onClick={assignTileHandler()}
+      >
+        {piece !== "blank" ? substituteIcon(piece, "huge") : null}
+      </div>
     );
+  };
+
+  const pieceSwitch = (turn) => {
+    const pieceSwitchKey = {
+      1: "yellow",
+      2: "red",
+    };
+    return pieceSwitchKey[turn];
+  };
+
+  const handleTileClick = (columnIndex) => {
+    if (!winner) {
+      const payload = {
+        columnIndex,
+        piece: pieceSwitch(turn),
+      };
+
+      if (playerNumber === -1) {
+        dispatch(connectFourPlace(payload));
+      } else {
+        const action = {
+          action: "place",
+          payload,
+        };
+        sendTurn(activeGameID, action, "connectFour", null);
+      }
+    }
+  };
+
+  const handleReceivedTurn = (response) => {
+    console.table(response);
+    // console.log("triggered.");
+    const { turn } = response;
+    if (response.turn && response.turn.game_type === "connectFour") {
+      const action = JSON.parse(turn.action);
+      switch (action.action) {
+        case "place":
+          dispatch(connectFourPlace(action.payload));
+          break;
+        case "reset":
+          dispatch(connectFourReset());
+          dispatch(setPlayer(null));
+          break;
+        default:
+          console.error("invalid connectFour turn received");
+      }
+    }
+  };
+
+  const headerSection = () => {
+    if (!playerNumber) {
+      return <h1>Select a player</h1>;
+    } else if (winner === "draw") {
+      return <h1>It's a Draw!</h1>;
+    } else if (winner) {
+      return (
+        <h1>
+          {substituteIcon(winner, "big")}
+          {` Wins!`}
+        </h1>
+      );
+    } else {
+      return <h1>{substituteIcon(pieceSwitch(turn), "big")}</h1>;
+    }
+  };
+
+  return (
+    <Segment.Group>
+      <PlayerControls myTurn={myTurn()} iconDetails={iconDetails} />
+      <Segment padded centered>
+        <ActionCable
+          channel={{ channel: "TurnsChannel", game_id: activeGameID }}
+          onReceived={handleReceivedTurn}
+          onConnected={() => console.log("C4 Turn Channel Connected")}
+          onDisconnected={() => console.log("C4 Turn Channel DCed")}
+        />
+        {headerSection()}
+        <div className="boardbox">{drawGrid()}</div>
+      </Segment>
+    </Segment.Group>
+  );
 };
 
 export default ConnectFour;
--- src/features/connectFour/connectFourSlice.js
+++ src/features/connectFour/connectFourSlice.js
@@ -2,44 +2,44 @@ import { createSlice } from "@reduxjs/toolkit";
 import { emptyGrid } from "./emptyGrid";
 
 export const connectFourSlice = createSlice({
-    name: "connectFour",
-    initialState: {
-        grid: emptyGrid,
-        turn: "1",
-        winner: null,
+  name: "connectFour",
+  initialState: {
+    grid: emptyGrid,
+    turn: "1",
+    winner: null,
+  },
+  reducers: {
+    connectFourPlace: (state, action) => {
+      const { columnIndex, piece } = action.payload;
+      const turnSwitch = { red: "1", yellow: "2" };
+      const oldColumn = state.grid[columnIndex];
+      let newColumn = oldColumn.filter((space) => space !== "blank");
+      let posY = 0;
+      // let didPlace = false;
+      if (newColumn.length < 6) {
+        newColumn.push(piece);
+        posY = newColumn.length - 1;
+        // didPlace = true;
+        state.turn = turnSwitch[piece];
+      }
+      while (newColumn.length < 6) {
+        newColumn.push("blank");
+      }
+
+      state.grid[columnIndex] = newColumn;
+
+      const posX = columnIndex;
+
+      state.winner = checkForWins(state.grid, posY, posX);
+
+      // state.turn = turnSwitch[piece];
     },
-    reducers: {
-        connectFourPlace: (state, action) => {
-            const { columnIndex, piece } = action.payload;
-            const turnSwitch = { red: "1", yellow: "2" };
-            const oldColumn = state.grid[columnIndex];
-            let newColumn = oldColumn.filter((space) => space !== "blank");
-            let posY = 0;
-            // let didPlace = false;
-            if (newColumn.length < 6) {
-                newColumn.push(piece);
-                posY = newColumn.length - 1;
-                // didPlace = true;
-                state.turn = turnSwitch[piece];
-            }
-            while (newColumn.length < 6) {
-                newColumn.push("blank");
-            }
-
-            state.grid[columnIndex] = newColumn;
-
-            const posX = columnIndex;
-
-            state.winner = checkForWins(state.grid, posY, posX);
-
-            // state.turn = turnSwitch[piece];
-        },
-        connectFourReset: (state) => {
-            state.grid = emptyGrid;
-            state.turn = "1";
-            state.winner = null;
-        },
+    connectFourReset: (state) => {
+      state.grid = emptyGrid;
+      state.turn = "1";
+      state.winner = null;
     },
+  },
 });
 
 export const { connectFourPlace, connectFourReset } = connectFourSlice.actions;
@@ -51,51 +51,47 @@ export const selectConnectFourWinner = (state) => state.connectFour.winner;
 export default connectFourSlice.reducer;
 
 const checkForWins = (grid, posY, posX) => {
-    const latestPiece = grid[posX][posY];
+  const latestPiece = grid[posX][posY];
 
-    const fourPieces = `${latestPiece}${latestPiece}${latestPiece}${latestPiece}`;
+  const fourPieces = `${latestPiece}${latestPiece}${latestPiece}${latestPiece}`;
 
-    const thisColumn = grid[posX];
+  const thisColumn = grid[posX];
 
-    const thisRow = grid.map((column) => column[posY]);
+  const thisRow = grid.map((column) => column[posY]);
 
-    const leftDiagonal = [];
-    for (let i = -5; i <= 5; i++) {
-        if (grid[posX + i]) { // check to see the column is present
-            leftDiagonal.push(grid[posX + i][posY + i]);
-        }
+  const leftDiagonal = [];
+  for (let i = -5; i <= 5; i++) {
+    if (grid[posX + i]) {
+      // check to see the column is present
+      leftDiagonal.push(grid[posX + i][posY + i]);
     }
+  }
 
-    const rightDiagonal = [];
-    for (let i = -5; i <= 5; i++) {
-        if (grid[posX - i]) { // check to see the column is present
-            rightDiagonal.push(grid[posX - i][posY + i]);
-        }
+  const rightDiagonal = [];
+  for (let i = -5; i <= 5; i++) {
+    if (grid[posX - i]) {
+      // check to see the column is present
+      rightDiagonal.push(grid[posX - i][posY + i]);
     }
+  }
 
-    const allLines = [thisRow, thisColumn, leftDiagonal, rightDiagonal];
+  const allLines = [thisRow, thisColumn, leftDiagonal, rightDiagonal];
 
-    //Check to see if there's a win in any of the lines
-    if (
-        allLines.some((line) => {
-            let lineString = line.join("");
-            return lineString.includes(fourPieces);
-        })
-    ) {
-        console.log(`${latestPiece} wins!`);
-        return latestPiece;
-    }
+  //Check to see if there's a win in any of the lines
+  if (
+    allLines.some((line) => {
+      let lineString = line.join("");
+      return lineString.includes(fourPieces);
+    })
+  ) {
+    console.log(`${latestPiece} wins!`);
+    return latestPiece;
+  }
 
-    //Check to see if the grid is full.
-    if (
-        grid.every(column => {
-            let upsideDownColumn = [...column].reverse() // check from the top of the column so we find empty spaces sooner
-            return upsideDownColumn.every(space => space != "blank") 
-        }
-            )
-    ) {
-        return "draw"
-    }
+  //Check to see if the grid is full.
+  if (grid.every((column) => column[6] != "empty")) {
+    return "draw";
+  }
 
-    return null;
+  return null;
 };
